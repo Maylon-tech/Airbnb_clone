01cbd852b5b45c77a9633a95c21c23a489d70496
Maylon-tech <minatokiroisen77@gmail.com>
2023-07-12 12:57:14 +0900

    starting MUI _ Vite-react Ecommerce

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+
+import { Button, Container } from '@mui/material'
+
+function App() {
+
+
+  return (
+    <>
+      <Container
+        maxWidth='xl'
+        sx={{
+          background: "#eee"
+        }}
+      >
+
+
+        {
+          /*
+
+            Appbar
+            Banner
+            Promotions
+            title
+            Products
+            footer
+            searchbox
+            appdrawer
+
+          */
+        }
+
+
+
+
+        <Button variant='contained'>Test</Button>
+      </Container>
+      
+    </>
+  )
+}
+
+export default App
